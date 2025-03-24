5cc43023bc88968efe43c67d639d902b529075a4
Clement R <104372704+IDEFIX95@users.noreply.github.com>
2025-03-24 15:15:38 +0100

    Add files via upload

---
+++ projet_java/projet_java/src/main/java/com/projet/projet_java/Categorie.java
@@ -0,0 +1,15 @@
+package com.projet.projet_java;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Date;
+
+public class Categorie {
+    private int idCategorie;
+    private String listeCategorie;
+
+    public Categorie(int idCategorie, String listeCategorie) {
+        this.idCategorie = idCategorie;
+        this.listeCategorie = listeCategorie;
+    }
+}
